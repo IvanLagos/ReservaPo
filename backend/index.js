98d0ebe8373e08a08d4b610dcd5886d7d07d8334
IvanLagos <lagosivan090@gmail.com>
2026-05-25 01:31:37 -0400

    secure cors production

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,13 @@ from "./routes/userRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://reserva-po.vercel.app"
+  ],
+  credentials: true
+}));
 
 app.use(express.json());
 
